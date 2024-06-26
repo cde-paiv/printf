401ec02336637c5f4c3df3d889ef98b814b27081
Christofer De Paiva Mota <cde-paiv@c2r3s8.42porto.com>
2024-06-26 19:22:54 +0100

    send

---
+++ ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-paiv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 16:21:22 by cde-paiv          #+#    #+#             */
+/*   Updated: 2024/05/22 08:46:49 by cde-paiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+# define UPPER_HEX "0123456789ABCDEF"
+# define LOWER_HEX "0123456789abcdef"
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar_pf(char c);
+int	ft_putstr_pf(char *s);
+int	format_printf(char mask, va_list ap);
+int	ft_putnbr_pf(int nbr);
+int	ft_unint_pf(unsigned int nbr);
+int	ft_hexa_pf(unsigned long n, char format);
+int	ft_putptr_pf(unsigned long nbr, char *base);
+int	hexlong(unsigned long nbr, char *base);
+
+#endif
